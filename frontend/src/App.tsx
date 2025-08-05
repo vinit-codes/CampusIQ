4f8804c8061d43249046b85175f92da44a479ae8
Naresh Mahapatra <codes.xdev@gmail.com>
2025-08-05 09:00:43 +0530

    added loading animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, useLocation } from "react-router-dom";
-import AppRouter from './Router';
-import Header from './Components/Common/Header';
+import AppRouter from "./Router";
+import Header from "./Components/Common/Header";
+import Loading from "./Components/Loading/loading.tsx";
 
 const AppWrapper: React.FC = () => {
   return (
@@ -13,14 +14,24 @@ const AppWrapper: React.FC = () => {
 
 const App: React.FC = () => {
   const location = useLocation();
-  const showHeaderPaths = ["/Dashboard", "/Portal", "/Profile" , "/Results", "/Fees", "/Hostel", "/Library"];
+  const showHeaderPaths = [
+    "/Dashboard",
+    "/Portal",
+    "/Profile",
+    "/Results",
+    "/Fees",
+    "/Hostel",
+    "/Library",
+  ];
   const shouldShowHeader = showHeaderPaths.includes(location.pathname);
 
   return (
-    <div className="min-h-screen w-screen">
-      {shouldShowHeader && <Header />}
-      <AppRouter />
-    </div>
+    <Loading>
+      <div className="min-h-screen w-screen">
+        {shouldShowHeader && <Header />}
+        <AppRouter />
+      </div>
+    </Loading>
   );
 };
 
