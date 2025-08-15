969d5806d670b73be21104106b1879ebf6e2b9e4
Naresh Mahapatra <codes.xdev@gmail.com>
2025-08-15 11:59:50 +0530

    new changes

---
+++ frontend/src/Pages/AcademicManagment/Attendance.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Attendance:React.FC = () => {
+  return (
+    <div>Attendance</div>
+  )
+}
+
+export default Attendance
--- frontend/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './../../Components/Common/Header.tsx';
 import Sidebar from './../../Components/Common/Sidebar.tsx';
-
+import facultyImg from '../../assets/Images/faculty.png';
+import Profile from './../AcademicManagment/StudentProfile.tsx';
 const Dashboard = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 text-gray-800">
@@ -10,12 +11,35 @@ const Dashboard = () => {
         <Sidebar />
         <main className="flex-1 ml-0 md:ml-60 px-6 lg:px-12 py-8 animate-fadeIn">
           <div className="max-w-7xl mx-auto">
-             <section className="flex h-[35vh]">
-              <div className="bg-gray-100 rounded-lg w-[350px] h-full flex justify-center item-center">
-                <h1 className="text-[20px] font-extrabold font-inter m-[4px]">Student-Faculty Information</h1>
-                
-                <img src='""' className=""/>
+
+             <section className="flex justify-end h-[40vh]">
+
+              {/* Student info */}
+              <div className="bg-gray-200 rounded-lg w-1/4 h-full flex flex-col items-center">
+              <Profile />
               </div>
+        
+
+              {/* Attendance */}
+              <div className="bg-gray-200 rounded-lg w-1/2 h-full flex flex-col items-center ml-4 mr-4 overflow-scroll">
+                <h1 className="text-[20px] font-extrabold font-inter m-1">Attendance  </h1>
+                <p className="text-[16px] font-semibold mt-2">Total Classes: 30</p>
+                <p className="text-[14px] text-gray-600">Classes Attended: 25</p>
+                <p className="text-[14px] text-gray-600">Attendance Percentage: 83%</p>
+              </div>
+
+              {/* Faculty Information */}
+              <div className="bg-gray-200 rounded-lg w-1/4 h-full flex flex-col items-center p-0 m-0">
+                <h1 className="text-[20px] font-extrabold font-inter m-1">Student-Faculty Information</h1>
+                <img src={facultyImg} alt="Faculty" className="h-[100px] w-[100px] rounded-full" />
+                <h3 className="text-[16px] font-semibold mt-2 italic text-[#2563EB]">Dr. Emily Rodriguez</h3>
+                <p className="text-[14px] text-gray-600">Professor of Computer Science</p>
+                <p className="text-[14px] text-gray-600">Office Hours: Mon-Fri, 10 AM - 3 PM</p>
+                <p className="text-[14px] text-gray-600">Email: xyz@school.ac.in</p>
+                <p className="text-[14px] text-gray-600">Phone: +91 12345 67890</p>
+                <p className="text-[14px] text-gray-600">Room No: 101, Block A</p>
+              </div>
+
              </section>
           </div>
         </main>
